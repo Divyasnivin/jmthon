ef3b69d08090562cad8a1b0222e8dbfb97eb3f90
Divyasnivin <103533746+Divyasnivin@users.noreply.github.com>
2022-07-08 00:02:25 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+from sample_config import Config
+
+
+class Development(Config):
+    #هنا ضع الكودات الخاصه بك امسح الكلام العربي وضعهن
+    APP_ID = 9365736
+    API_HASH =  "3fa1b478ca72a73d267e10188556cc34"
+    ALIVE_NAME = "هادي"
+    DB_URI = "postgres://hbifpcsx:H1KGLnvFlLPFs5u9g6tjhNb9bbdA8zmc@salt.db.elephantsql.com/hbifpcsx"
+    STRING_SESSION = "1BJWap1sBu2aa-W-Z1BWaa3I8qUnfQvGaTwn8fZUK-sMd0UXPwDUopOa_4Xb1_GDYLCtd91p7Iq_5ohBUoZE6H32taEl63AbxHGBO6EBTxqfhv6a4DU1gAvFdwJlGZ7tlmRXVPQHZfowmtBbaqoetNFJmTe0A_b5S8zDtPrX7jUKTByxnzZMwmaGwoNNoS_gpMb0TXXrj_2brVv5li1DS0gD76VLv7Tts3c3n40mO7LwqmVW3tzl6gnhq_9px86IXR7afcuaxNhbu_zu7zlXdC9kbVkaU_PdpG4Al7PWZXdrL522dTXQTVj1AeujPsernbvou8f-O4qRIASbAirZeNCfZGYodzyo="
+    TG_BOT_TOKEN = "5111897157:AAGKx8QRGetJKugii06AwtLoHqE51AyYMbs"
+    TG_BOT_USERNAME = "q71bot"
+    PRIVATE_GROUP_BOT_API_ID = -1001659591037
+    COMMAND_HAND_LER = "."
+    SUDO_USERS = [5049024596]
+    SUDO_COMMAND_HAND_LER = "،"
+    TZ = "Asia/Baghdad"
+#t.me/jmthon
